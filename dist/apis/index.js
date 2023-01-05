dcfc3a70622d5d35406f0c1254f71bc1941aa3cc
Brad Johnson <bradsk88@gmail.com>
2023-01-04 23:36:48 -0600

    Initial generation (with some manual fixes)

---
+++ dist/apis/index.js
@@ -0,0 +1,42 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./AboutApi"), exports);
+__exportStar(require("./AccountsApi"), exports);
+__exportStar(require("./AttachmentsApi"), exports);
+__exportStar(require("./AutocompleteApi"), exports);
+__exportStar(require("./AvailableBudgetsApi"), exports);
+__exportStar(require("./BillsApi"), exports);
+__exportStar(require("./BudgetsApi"), exports);
+__exportStar(require("./CategoriesApi"), exports);
+__exportStar(require("./ChartsApi"), exports);
+__exportStar(require("./ConfigurationApi"), exports);
+__exportStar(require("./CurrenciesApi"), exports);
+__exportStar(require("./DataApi"), exports);
+__exportStar(require("./InsightApi"), exports);
+__exportStar(require("./LinksApi"), exports);
+__exportStar(require("./ObjectGroupsApi"), exports);
+__exportStar(require("./PiggyBanksApi"), exports);
+__exportStar(require("./PreferencesApi"), exports);
+__exportStar(require("./RecurrencesApi"), exports);
+__exportStar(require("./RuleGroupsApi"), exports);
+__exportStar(require("./RulesApi"), exports);
+__exportStar(require("./SearchApi"), exports);
+__exportStar(require("./SummaryApi"), exports);
+__exportStar(require("./TagsApi"), exports);
+__exportStar(require("./TransactionsApi"), exports);
+__exportStar(require("./UsersApi"), exports);
+__exportStar(require("./WebhooksApi"), exports);
